316f9ae75163260e05f3ee03260947e779864a65
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-01-07 19:13:56 +0600

    Initial commit

---
+++ app/src/main/java/edu/daffodilvarsity/a2zservice/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.daffodilvarsity.a2zservice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
