3fd8a50fb5a6656e87c23ab7dee5ed1ee231289d
Rs Robin <srrobin33@gmail.com>
2020-01-08 16:53:25 +0600

    Tab Layout in  Home Activity

--- app/src/main/java/edu/daffodilvarsity/a2zservice/HomeFragment.java
+++ app/src/main/java/edu/daffodilvarsity/a2zservice/HomeFragment.java
@@ -4,10 +4,15 @@ package edu.daffodilvarsity.a2zservice;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
@@ -15,6 +20,7 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    View view;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,7 +31,54 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        setupTabView();
+        getActivity().setTitle("Home");
+
+        return view;
     }
+
+    private void setupTabView() {
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        ViewPager mViewPager = view.findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+}
+
+ class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+    private Fragment fragment = null;
+
+    SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                fragment = new QuickInfoFragment();
+                break;
+            case 1:
+                fragment = new ServiceProviderFragment();
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+
 
 }
