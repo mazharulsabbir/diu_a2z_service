01db7688be4b8aa022c55ba3318ac518c821cb62
Rs Robin <srrobin33@gmail.com>
2020-01-13 14:14:55 +0600

    navigation click listener

--- app/src/main/java/edu/daffodilvarsity/a2zservice/MainActivity.java
+++ app/src/main/java/edu/daffodilvarsity/a2zservice/MainActivity.java
@@ -1,22 +1,47 @@
 package edu.daffodilvarsity.a2zservice;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         openFragment(new HomeFragment());
+
+        bottomNavigationView=findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId()==R.id.nav_more){
+                    openFragment(new ProfileFragment());
+                    return true;
+                }
+                if (item.getItemId()==R.id.nav_home){
+                    openFragment(new HomeFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
+
 
     private void openFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, fragment)
+                .replace(R.id.fragment_container, fragment)
                 .commit();
     }
 }
